9d8685d56641034c4ea13edefa366b309b10cb10
mosuromayowa <mosuromayowa@gmail.com>
2023-11-21 17:26:26 -0500

    making some adjustments on the CSS

--- conversion.js
+++ conversion.js
@@ -20,7 +20,10 @@ function convertCurrency() {
         result = amount * CAD_TO_USD_RATE;
     } else if (fromCurrency === 'USD' && toCurrency === 'CAD') {
         result = amount * USD_TO_CAD_RATE;
-  
+    } else {
+        alert('Invalid conversion selection.');
+        return;
+    }
 
     
     document.getElementById('result').innerText = `Result: ${result.toFixed(3)} ${toCurrency}`;
